43c26f45466565406e4dda2699465ac2c8bf7628
eliandro <eliandro.goncalves1@gmail.com>
2019-03-04 13:36:32 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Rec_Gui frame=new Rec_Gui();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
